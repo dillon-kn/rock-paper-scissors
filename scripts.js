bb4b9fe3fd4970861c4a62caf904592e05744d88
Dillon Nguyen <iamdnguyen1024@gmail.com>
2022-02-15 20:49:05 -0800

    Completed entire UI - forgot to commit diffs

--- scripts.js
+++ scripts.js
@@ -1,62 +1,166 @@
+// TODO
+// Update score after each win
+// Stop at 5 wins
+// Show winner
+
 const winnerKey = {
     "rock": "scissors",
     "scissors": "paper",
     "paper": "rock"
 }
 
-function computerPlay() {
-    let options = ["Rock", "Paper", "Scissors"];
+const currentScore = {
+    "kanojia": 0,
+    "potential-convert": 0
+}
+
+const game = () => {
+    let userPoints = 0;
+    let computerPoints = 0;
+
+    const choices = document.querySelectorAll('.choice');
+    choices.forEach((choice) => {
+            choice.addEventListener('click', playRound)
+        }
+    )
+}
+
+const generateComputerMove = () => {
+    const options = ["rock", "paper", "scissors"];
+
+    // Chooce random number between 0 and 2 and pick the corresponding index
     return options[Math.floor(Math.random() * 3)];
 }
 
-function getUserMove() {
-    let move = prompt("Choose and type 'rock', 'paper', or 'scissors'!")
-    return move
-}
-
-function playRound() {
-    let computerMove = computerPlay().toLowerCase();
-    let userMove = getUserMove().toLowerCase();
-    while (userMove === null || !winnerKey.hasOwnProperty(userMove)) {
-        userMove = getUserMove().toLowerCase();
+const playRound = (choice) => {
+    
+    // Get moves
+    let computerMove = generateComputerMove();
+    let userMove = choice.target.id;
+    if (userMove === null || !winnerKey.hasOwnProperty(userMove)) {
+        return
     }
 
+    // Add result div if not yet existent
+    if (!document.querySelector(".result")) {
+        const choiceContainer = document.querySelector('.choice-container');
+
+        let result = document.createElement('div');
+        result.classList.add('result');
+        choiceContainer.insertBefore(result, document.querySelector('.score-outer-container'));
+    }
+
+    let winner = '';
+    let result = document.querySelector('.result');
+
+    // Display correct result
     if (computerMove === userMove) {
-        console.log(`It's a tie! Both of you played ${title(computerMove)}`);
-        return "TIE";
+        result.textContent = `
+            A tie! You both picked ${title(computerMove)}! Try Again!
+        `
     }
+
     else if (winnerKey[computerMove] === userMove) {
-        console.log(`You lose! ${title(computerMove)} beats ${title(userMove)}!`);
-        return "COMPUTER_WIN";
+        winner = "kanojia"
+        result.textContent = `
+            He read your mind! He knew you would play ${title(userMove)} and beat you with ${title(computerMove)}!
+        `
     }
-    console.log(`You win! ${title(userMove)} beats ${title(computerMove)}!`);
-    return "USER_WIN";
-}
 
-function game() {
-    let userPoints = 0;
-    let computerPoints = 0;
-    while (userPoints < 3 && computerPoints < 3) {
-        result = playRound();
-        if (result === "USER_WIN") {
-            userPoints++;
-        }
-        else if (result === "COMPUTER_WIN") {
-            computerPoints++;
-        }
-    }
-    printWinner(userPoints, computerPoints);
-}
-
-function printWinner(userPoints, computerPoints) {
-    if (userPoints > computerPoints) {
-        console.log(`You win! The score was ${userPoints}-${computerPoints}`);
-    }
     else {
-        console.log(`You win! The score was ${userPoints}-${computerPoints}`);
+        winner = "potential-convert";
+        result.textContent = `You did it! You psychoanalyzed him and predicted he would play ${title(computerMove)}! You're good at this!`
+    }
+
+    incrementScore(winner);
+    showScore()
+
+    // Check if game end
+    checkGameEnd();
+}
+
+const incrementScore = (winner) => {
+    if (currentScore.hasOwnProperty(winner)) {
+        ++currentScore[winner]
     }
 }
 
-function title(string) {
+const showScore = () => {
+    const scores = document.querySelectorAll('.score')
+    scores.forEach((score) => {
+            let scoreValue = score.querySelector('h3')
+            scoreValue.textContent = +currentScore[scoreValue.id];
+        }
+    )
+}
+
+const checkGameEnd = () => {
+    // If someone reaches 5 points
+    potentialConvertScore = currentScore['potential-convert'];
+    kanojiaScore = currentScore['kanojia'];
+    if (potentialConvertScore === 5 || kanojiaScore === 5) {
+
+        const finalResult = document.createElement('h2');
+        finalResult.setAttribute('id', 'final-result')
+        const choiceContainer = document.querySelector('.choice-container')
+
+        if (potentialConvertScore === 5) {
+            finalResult.textContent = "Congrats! You fought off Kult Leader Kanojia's Kunning Kharisma and avoided Konversion!!!";
+        }
+        else {
+            finalResult.textContent = "Dah! You were inKapable of Kombating Kult Leader Kanojia's Kunning Kharisma and Konsented to Konversion!!!";
+        }
+        choiceContainer.appendChild(finalResult)
+
+        // Turn off all buttons and create a button to reset the page
+        turnOffButtons();
+        addResetButton();
+
+    }
+}
+
+const turnOffButtons = () => {
+    const choices = document.querySelectorAll('.choice');
+    choices.forEach((choice) => {
+            choice.removeEventListener('click', playRound)
+        }
+    )
+}
+
+const addResetButton = () => {
+    const resetButton = document.createElement('button');
+    resetButton.classList.add('reset-btn');
+    resetButton.textContent = "Reset";
+    resetButton.addEventListener('click', resetPage);
+    const choiceContainer = document.querySelector('.choice-container');
+    choiceContainer.appendChild(resetButton);
+}
+
+const resetPage = () => {
+    // Reset scores
+    currentScore['potential-convert'] = 0;
+    currentScore['kanojia'] = 0;
+    showScore();
+
+    // Remove result div, final result div, and reset button
+    const result = document.querySelector('.result');
+    const finalResult = document.querySelector('#final-result');
+    const resetButton = document.querySelector('.reset-btn');
+    const choiceContainer = document.querySelector('.choice-container');
+    choiceContainer.removeChild(result);
+    choiceContainer.removeChild(finalResult);
+    choiceContainer.removeChild(resetButton);
+
+    // Turn buttons back on
+    const choices = document.querySelectorAll('.choice');
+    choices.forEach((choice) => {
+            choice.addEventListener('click', playRound)
+        }
+    )
+}
+
+const title =  (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+game()
